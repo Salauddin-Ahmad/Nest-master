c90e7436e143bdd3bf9fe70131e46271d91eafec
Md. Salauddin <salauddinahmed555@gmail.com>
2025-07-16 21:10:29 +0600

    Learned how to post with raw json and httpyac

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,26 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get(':id')
-  @Get(':id/:optional')
-  public getUsers(@Param('id') id: any, @Param('optional') optional?: any) {
+  @Get([':id', ':id/:optional'])
+  public getUsers(
+    @Param('id') id: any,
+    @Param('optional') optional?: any,
+    @Query() query?: any,
+  ) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    console.log({ id, optional });
+    const { limit, offset } = query;
+    console.log(limit, offset);
+    console.log(id, optional);
+
     return optional
       ? `You asked for id=${id} with optional=${optional}`
       : `You asked for id=${id} only`;
   }
 
   @Post()
-  public createUsers() {
+  public createUsers(@Body() request: any) {
+    console.log(request);
     return 'you sent a post request to /users';
   }
 }
