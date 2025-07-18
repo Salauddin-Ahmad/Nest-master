3a187f937f9d4be6d64b75c02a02f3bc81f02827
Md. Salauddin <salauddinahmed555@gmail.com>
2025-07-19 00:42:50 +0600

    Query param validation and pipes basics

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,21 +8,35 @@ import {
   Headers,
   Req,
   Ip,
+  ParseIntPipe,
+  DefaultValuePipe,
 } from '@nestjs/common';
 
 import { Request } from 'express';
 @Controller('users')
 export class UsersController {
+  /****
+   *
+   *    * Final Endpoint - /users/id?limit=10&page=1 
+   * /users/1223 -> returns one user whos id is 1234
+   * /users?limit=10&page=2 -> return page 2 with limt of pagination 10
+   * Parama id - optional, convert to integer, cannot have a default value
+   * USE CASES ==>
+   * Query limit - integer, default 10
+   * /users/ -> return all users with default pagination
+   * Query page - integer, default value 1
+
+   */
+
   @Get([':id', ':id/:optional'])
   public getUsers(
-    @Param('id') id?: any, // just to grab the id from the URL
-    @Param('optional') optional?: any,
-    @Query() query?: any,
+    @Param('id', ParseIntPipe) id?: number, // using ParseIntPipe to ensure id is an integer
+    @Param('optional') optional?: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit?: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page?: number,
   ) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { limit, offset } = query;
-    console.log(limit, offset);
-    console.log(id, optional);
+    console.log(limit, page);
+    console.log(typeof limit, typeof page);
 
     return optional
       ? `You asked for id=${id} with optional=${optional}`
