82c531eb2ce151255552ab94dcdfb9961726df18
Md. Salauddin <salauddinahmed555@gmail.com>
2025-07-23 01:26:53 +0600

    Enhance CreatePostDto with ApiProperty decorators for better Swagger documentation

--- src/post/dtos/create-post.dto.ts
+++ src/post/dtos/create-post.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   IsArray,
@@ -17,14 +18,14 @@ export enum PostType {
   Post = 'post',
   Page = 'page',
   Story = 'story',
-  Series = 'series'
+  Series = 'series',
 }
 
 export enum postStatus {
   Draft = 'draft',
   Scheduled = 'scheduled',
   Review = 'review',
-  Published = 'published'
+  Published = 'published',
 }
 
 export class CreatePostMetaOptionsDto {
@@ -37,16 +38,29 @@ export class CreatePostMetaOptionsDto {
   value: string;
 }
 
+// MARK: CreatePostDto
 export class CreatePostDto {
+  @ApiProperty({
+    description: 'This is title of the blog post',
+    example: 'This is a title',
+  })
   @IsString()
   @MinLength(3)
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    enum: PostType,
+    description: "Possible values, 'post', 'page', 'story', 'series'",
+  })
   @IsNotEmpty()
   @IsEnum(PostType)
   postType: PostType;
 
+  @ApiProperty({
+    description: "For example - 'my-url'",
+    example: 'my-blog-post',
+  })
   @IsString()
   @IsNotEmpty()
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
@@ -55,9 +69,18 @@ export class CreatePostDto {
   })
   slug: string;
 
+  @ApiProperty({
+    enum: postStatus,
+    description: "Possible values, 'draft', 'scheduled', 'review', 'published'",
+  })
   @IsEnum(postStatus)
+  @IsNotEmpty()
   status: postStatus;
 
+  @ApiPropertyOptional({
+    description: 'Content of the post',
+    example: 'This is the content of the post',
+  })
   @IsString()
   @IsOptional()
   content?: string;
